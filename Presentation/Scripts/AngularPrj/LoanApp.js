bee496ff8fcfa89d271a1d96d7e2020575b5e302
bordline <clyde.uyenghua@live.com>
2015-08-19 10:29:56 +0800

    Moved commits from TFS to git.

--- Presentation/Scripts/AngularPrj/LoanApp.js
+++ Presentation/Scripts/AngularPrj/LoanApp.js
@@ -1,3 +1,12 @@
-﻿var LoanApp = angular.module("LoanApp", ['ngRoute']);
+﻿///<reference path="../angular.min.js" />
+///<reference path="../angular-route.min.js" />
+var LoanApp = angular.module("LoanApp", ['ngRoute']);
 
-var test
+LoanApp.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
+    $routeProvider.when('/loanCalculator', {
+        templateUrl: 'Views/LoanCalculator.html',
+        controller: 'LoanCalculatorController'
+    });
+    $routeProvider.otherwise({ redirectTo: '/loanCalculator' });
+    $locationProvider.html5Mode(true);
+}]);
