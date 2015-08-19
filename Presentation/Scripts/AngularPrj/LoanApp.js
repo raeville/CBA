328d364222b0a3483267d2390ee02ee6ab27d6de
Raymund <atbelarmino/Gr@phite9441>
2015-08-19 09:25:38 +0800

    create new Loan Api Constant Connection

--- Presentation/Scripts/AngularPrj/LoanApp.js
+++ Presentation/Scripts/AngularPrj/LoanApp.js
@@ -1,1 +1,5 @@
 ﻿var LoanApp = angular.module("LoanApp", ['ngRoute']);
+var ConsConnLoanApi = angular.module('ConsConLoanApi', [])
+{
+    ConsConnLoanApi.constant("http://localhost:50671/", loanApiConsUrl);
+}
