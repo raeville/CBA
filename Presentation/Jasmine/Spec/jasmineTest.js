cf2f3d3a3157c51cb86082eb16ebaa87695a133b
Raymund <atbelarmino/Gr@phite9441>
2015-08-19 15:29:23 +0800

    Updated jasmineTest.js and SpecRunner.html

--- Presentation/Jasmine/Spec/jasmineTest.js
+++ Presentation/Jasmine/Spec/jasmineTest.js
@@ -2,6 +2,12 @@
 (function () {
     'use strict';
 
-   
-})();
+    describe('Loan Application', function () {
+        beforeEach(module('LoanApp'));
 
+        it('Is API config should be declare;', inject(function (loanApiConsUrl) {
+            expect(loanApiConsUrl).toBe('http://localhost:50671/');
+        }));
+    });
+
+})();
