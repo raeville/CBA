8c85c15eb1b2f64dde12324a65c98b16ad30edb3
Medenilla, Jay A. H. <jay.a.h.medenilla@accenture.com>
2015-08-20 19:35:29 +0800

    JAM 8/20: Fix the Calculation erro for addition. Used Parseint to evaluate var results to int.

--- Presentation/Scripts/AngularPrj/Controller/LoanCalculator.Controller.js
+++ Presentation/Scripts/AngularPrj/Controller/LoanCalculator.Controller.js
@@ -7,21 +7,18 @@
     $scope.calculateLoan = function () {
         var amountOfTheLoan = $scope.amountLoan;
         var annualInterest = $scope.annualInterest;
-        var repaymentPeriod = $scope.repaymentPeriod;
+        var repaymentPeriod = $scope.repaymentPeriod * 12;
 
         if (amountOfTheLoan.length != 0 && annualInterest.length != 0 && repaymentPeriod.length != 0) {
-            InterestResults = CalculatorService.multiply($scope.amountLoan, CalculatorService.divide(annualInterest, 100));
-            Balance = CalculatorService.add($scope.amountLoan, InterestResults);
-            $scope.monthlyPayment = CalculatorService.divide(Balance, $scope.repaymentPeriod);
+            Interest = CalculatorService.divide(annualInterest, 100);
+            InterestResults = CalculatorService.multiply($scope.amountLoan, Interest);
+            Balance = CalculatorService.add(parseInt($scope.amountLoan), parseInt(InterestResults));
+            $scope.monthlyPayment = CalculatorService.divide(Balance, parseInt(repaymentPeriod));
             $scope.totalPayment = Balance;
             $scope.totalInterest = InterestResults;
             $scope.hideResults = false;
-
         }
-       
-       
-    
-
+                  
     };
 
 });
