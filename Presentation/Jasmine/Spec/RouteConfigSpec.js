db3efefec43c9f29874151ac2ff687fc63fbf82f
bordline <clyde.uyenghua@live.com>
2015-08-20 19:00:30 +0800

    Unit Test for Angular route config

---
+++ Presentation/Jasmine/Spec/RouteConfigSpec.js
@@ -0,0 +1,24 @@
+﻿/// <reference path="../../Scripts/angular.min.js" />
+/// <reference path="../../Scripts/angular-mocks.js" />
+/// <reference path="../../Scripts/angular-route.min.js" />
+/// <reference path="../lib/jasmine.js" />
+/// <reference path="../../Scripts/AngularPrj/LoanApp.js" />
+
+it('should map routes to controllers', function () {
+    module('LoanApp');
+
+    inject(function ($route) {
+
+        expect($route.routes['/home'].controller).toBe('HomeController');
+        expect($route.routes['/home'].templateUrl).
+          toEqual('Views/Home.html');
+
+        expect($route.routes['/loanCalculator'].templateUrl).
+          toEqual('Views/LoanCalculator.html');
+        expect($route.routes['/loanCalculator'].controller).
+          toEqual('LoanCalculatorController');
+
+        // otherwise redirect to
+        expect($route.routes[null].redirectTo).toEqual('/home')
+    });
+});
