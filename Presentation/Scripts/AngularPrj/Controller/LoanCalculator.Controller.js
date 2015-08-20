f07032e0f707b2f1c8ca9ee5d48ea96e2895028b
Raymund <atbelarmino/Gr@phite9441>
2015-08-20 17:14:01 +0800

    Add loan calculator services JS and edit some script in LoanCalculator.Controller.js

--- Presentation/Scripts/AngularPrj/Controller/LoanCalculator.Controller.js
+++ Presentation/Scripts/AngularPrj/Controller/LoanCalculator.Controller.js
@@ -1,23 +1,26 @@
-﻿'use strict';
-
-LoanApp.controller('LoanCalculatorController', function ($scope, $location) {
-    
+﻿LoanApp.controller('LoanCalculatorController', function ($scope, $location, CalculatorService) {
+    $scope.onlyNumbers = /^\d+$/;
     $scope.init = function () {
         $scope.hideResults = true;        
     }
 
     $scope.calculateLoan = function () {
-
         var amountOfTheLoan = $scope.amountLoan;
         var annualInterest = $scope.annualInterest;
         var repaymentPeriod = $scope.repaymentPeriod;
+
+        if (amountOfTheLoan.length != 0 && annualInterest.length != 0 && repaymentPeriod.length != 0) {
+            InterestResults = CalculatorService.multiply($scope.amountLoan, CalculatorService.divide(annualInterest, 100));
+            Balance = CalculatorService.add($scope.amountLoan, InterestResults);
+            $scope.monthlyPayment = CalculatorService.divide(Balance, $scope.repaymentPeriod);
+            $scope.totalPayment = Balance;
+            $scope.totalInterest = InterestResults;
+            $scope.hideResults = false;
+
+        }
        
-
-        $scope.monthlyPayment = add;
-        $scope.totalPayment = payments;
-        $scope.totalInterest = interest;
-
-        $scope.hideResults = false;
+       
+    
 
     };
 
