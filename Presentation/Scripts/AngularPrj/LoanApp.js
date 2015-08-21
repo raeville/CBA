12864a4e3e68884cf8fad9c03c05b64ff08d89a4
Rue Rae <raeville7@gmail.com>
2015-08-21 17:28:33 +0800

    test

--- Presentation/Scripts/AngularPrj/LoanApp.js
+++ Presentation/Scripts/AngularPrj/LoanApp.js
@@ -5,16 +5,21 @@ var LoanApp = angular.module("LoanApp", ['ngRoute']);
 LoanApp.constant("loanApiConsUrl", "http://localhost:50671/");
 
 LoanApp.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
-    $routeProvider.when('/home', {
-        templateUrl: 'Views/Home.html',
-        controller: 'HomeController'
+    $routeProvider.when('/Test', {
+        templateUrl: 'Views/Test.html',
+        controller: 'LoanCalculatorController'
     });
+    //$routeProvider.when('/home', {
+    //    templateUrl: 'Views/Index.html',
+    //    controller: 'HomeController'
+    //});
     $routeProvider.when('/loanCalculator', {
         templateUrl: 'Views/LoanCalculator.html',
         controller: 'LoanCalculatorController'
     });
     $routeProvider.when('/Register', {
         templateUrl: 'Views/Register.html',
+        controller: 'LoanCalculatorController'
     });
     $routeProvider.otherwise({ redirectTo: '/home' });
     //$locationProvider.html5Mode(true);
