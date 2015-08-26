880d1bc1331fad83eef9330622fb8c86407d55c0
bordline <clyde.uyenghua@live.com>
2015-08-26 16:40:50 +0800

    LoginService and UT.

---
+++ Presentation/Jasmine/Spec/LoginServiceSpec.js
@@ -0,0 +1,32 @@
+﻿/// <reference path="../../Scripts/angular.min.js" />
+/// <reference path="../../Scripts/angular-mocks.js" />
+/// <reference path="../lib/jasmine.js" />
+/// <reference path="../../Scripts/AngularPrj/LoanApp.js" />
+/// <reference path="../../Scripts/AngularPrj/Service/LoginService.js" />
+
+describe('log-in service', function () {
+    beforeEach(module('LoanApp'));
+
+    var loginService, $httpBackend;
+
+    beforeEach(inject(function ($injector) {
+        loginService = $injector.get('loginService');
+        $httpBackend = $injector.get('$httpBackend');
+    }));
+
+    it('should have a Token api call', function () {
+        var data = {
+            "email": "test@cba.com",
+            "password": "password",
+            "grant_type": "password"
+        };
+        $httpBackend.expectPOST('/Token/', data).respond(201, '');
+        loginService.authenticate(data);
+        $httpBackend.flush();
+    });
+
+    afterEach(function(){
+        $httpBackend.verifyNoOutstandingExpectation();
+        $httpBackend.verifyNoOutstandingRequest();
+    });
+});
---
+++ Presentation/Scripts/AngularPrj/Service/LoginService.js
@@ -0,0 +1,10 @@
+﻿///<reference path="../../angular.min.js" />
+LoanApp.service('loginService', function loginService($http) {
+    var authenticate = function (info) {
+        return $http({ method: 'POST', url: '/Token/', data: info });
+    };
+
+    return {
+        authenticate: authenticate
+    }
+});
