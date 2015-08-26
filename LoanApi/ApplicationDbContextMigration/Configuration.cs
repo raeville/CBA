f13a05db616c3bc4834e98de12966cbf9f4ef9a1
Medenilla, Jay A. H. <jay.a.h.medenilla@accenture.com>
2015-08-26 19:08:10 +0800

    JAM 8/26: Brought back the admin seed from migration folder.

--- LoanApi/ApplicationDbContextMigration/Configuration.cs
+++ LoanApi/ApplicationDbContextMigration/Configuration.cs
@@ -4,6 +4,9 @@ namespace LoanApi.ApplicationDbContextMigration
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using LoanApi.Models;
 
     internal sealed class Configuration : DbMigrationsConfiguration<LoanApi.Models.ApplicationDbContext>
     {
@@ -27,6 +30,25 @@ namespace LoanApi.ApplicationDbContextMigration
             //      new Person { FullName = "Rowan Miller" }
             //    );
             //
+
+            var hashit = new PasswordHasher();
+
+            var adduser = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(new ApplicationDbContext()));
+
+            //Admin
+            if (!adduser.Users.Any(r => r.UserName == "admin@cba.com"))
+            {
+                var admin = new ApplicationUser()
+                {
+                    UserName = "admin@cba.com",
+                    Email = "admin@cba.com",
+                    EmailConfirmed = true,
+                    PasswordHash = hashit.HashPassword("P@ssw0rd")
+                };
+
+                adduser.Create(admin);
+            }
+
         }
     }
 }
