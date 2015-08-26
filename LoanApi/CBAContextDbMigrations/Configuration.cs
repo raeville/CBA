f4a1c07291b4fc20da34621004fc6f7a504d26d2
atbelarmino <altorrebelarmino@accenture.com>
2015-08-26 08:22:51 +0800

    Update LoanAPI Seeding

--- LoanApi/CBAContextDbMigrations/Configuration.cs
+++ LoanApi/CBAContextDbMigrations/Configuration.cs
@@ -4,29 +4,36 @@ namespace LoanApi.CBAContextDbMigrations
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using LoanApi.Models;
 
     internal sealed class Configuration : DbMigrationsConfiguration<LoanApi.Models.CBAContextDb>
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = false;
-            MigrationsDirectory = @"CBAContextDbMigrations";
+            AutomaticMigrationsEnabled = true;
+            MigrationsDirectory = "LoanApi.Models.CBAContextDb";
         }
 
         protected override void Seed(LoanApi.Models.CBAContextDb context)
         {
-            //  This method will be called after migrating to the latest version.
+            var hashit = new PasswordHasher();
+            var adduser = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(new ApplicationDbContext()));            
 
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
+            //Admin
+            if (!adduser.Users.Any(r => r.UserName == "admin@cba.com"))
+            {             
+                var admin = new ApplicationUser()
+                {
+                    UserName = "admin@cba.com",
+                    Email = "admin@cba.com",
+                    EmailConfirmed = true,
+                    PasswordHash = hashit.HashPassword("P@ssw0rd")
+                };
+                adduser.Create(admin);
+
+            }
         }
     }
 }
