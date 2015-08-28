5475f2eefa9529d7922aa53aa32c33d04c664d22
bordline <clyde.uyenghua@live.com>
2015-08-28 12:53:31 +0800

    Updated authenticate service to use loanApiConsUrl constant service.

--- Presentation/Scripts/AngularPrj/Service/AspNetUserService.js
+++ Presentation/Scripts/AngularPrj/Service/AspNetUserService.js
@@ -1,10 +1,12 @@
 ﻿///<reference path="../../angular.min.js" />
-angular.module("LoanApp").service('AspNetUser', function loginService($http) {
-    var authenticate = function (info) {
-        return $http({ method: 'POST', url: '/Token/', data: info });
+///<reference path="../LoanApp.js" />
+angular.module("LoanApp").service('AspNetUser', function loginService($http, loanApiConsUrl) {
+    var authenticate = function (user) {
+        var URL = loanApiConsUrl + 'Token/';
+        return $http({ method: 'POST', url: URL, data: user });
     };
 
     return {
-        authenticate: authenticate
+        login: authenticate
     }
 });
