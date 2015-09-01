e38cd07062f30af421efdae430f47438255f0c79
Medenilla, Jay A. H. <jay.a.h.medenilla@accenture.com>
2015-09-01 19:01:26 +0800

    JAM 9/1: Controller for login and register

--- Presentation/Scripts/AngularPrj/Controller/LogIn.Controller.js
+++ Presentation/Scripts/AngularPrj/Controller/LogIn.Controller.js
@@ -1,6 +1,19 @@
-﻿LoanApp.controller('LoginController', function ($scope, $location, loginService) {
+﻿LoanApp.controller('LoginController', function ($scope, $location, AspNetUser) {
+
+    var sigIn = function () {
+        var user = [];
+        var username = $scope.user.username;
+        var password = $scope.user.password;
 
 
+        var user = {
+            "username": username,
+            "password": password
+        };
 
+        angular.injector(['ng', 'LoanApp']).get("AspNetUser").login(user).success(function () { console.log(response); })
+  .error(function () { console.log("Error!") });
 
+    };
+    
 });
---
+++ Presentation/Scripts/AngularPrj/Controller/Registration.Controller.js
@@ -0,0 +1,19 @@
+﻿LoanApp.controller('RegistrationController', function ($scope, $location, AspNetUser) {
+
+    var registerUser = function () {
+        var user = [];
+        var email = $scope.userData.inputEmail;
+        var password = $scope.userData.inputPassword;
+        var confirmpassword = $scope.userData.inputConfirmPassword;
+
+        var user = {
+            "Email": email,
+            "Password": password,
+            "confirmPassword": confirmpassword
+        };
+        angular.injector(['ng', 'LoanApp']).get("AspNetUser").registerUser(user).success(function () { console.log(response); })
+    .error(function () { console.log("Error!") });
+
+    };
+    
+});
