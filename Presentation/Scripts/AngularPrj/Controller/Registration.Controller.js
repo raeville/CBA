a88c75665d59a56cea124d084752c5bd85dffcf4
Medenilla, Jay A. H. <jay.a.h.medenilla@accenture.com>
2015-09-01 19:41:46 +0800

    JAM 9/1: Commit Further updates

--- Presentation/Scripts/AngularPrj/Controller/Registration.Controller.js
+++ Presentation/Scripts/AngularPrj/Controller/Registration.Controller.js
@@ -1,4 +1,6 @@
-﻿LoanApp.controller('RegistrationController', function ($scope, $location, AspNetUser) {
+﻿
+LoanApp.controller('RegistrationController', ['$scope', 'AspNetUser', function ($scope, AspNetUser) {
+    // Do something with myService
 
     var registerUser = function () {
         var user = [];
@@ -10,10 +12,34 @@
             "Email": email,
             "Password": password,
             "ConfirmPassword": confirmpassword
-        };
-        angular.injector(['ng', 'LoanApp']).get("AspNetUser").registerUser(user).success(function () { console.log(response); })
-    .error(function () { console.log("Error!") });
+        }
 
+        AspNetUser.registerUser(user);
     };
+        
+}]);
+
+
+//LoanApp.controller('RegistrationController', function ($scope, $location, AspNetUser) {
+
+//    var registerUser = function () {
+//        var user = [];
+//        var email = $scope.userData.inputEmail;
+//        var password = $scope.userData.inputPassword;
+//        var confirmpassword = $scope.userData.inputConfirmPassword;
+
+//        var user = {
+//            "Email": email,
+//            "Password": password,
+//            "ConfirmPassword": confirmpassword
+//        };
+
+
+//        AspNetUser.registerUser(user);
+
+//    //    angular.injector(['ng', 'LoanApp']).get("AspNetUser").registerUser(user).success(function () { console.log(response); })
+//    //.error(function () { console.log("Error!") });
+
+//    };
     
-});
+//});
