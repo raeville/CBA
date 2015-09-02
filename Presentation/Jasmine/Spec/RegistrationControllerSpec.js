7a46986bdebf472d7ddeb186606f1d3ea903e6ee
Medenilla, Jay A. H. <jay.a.h.medenilla@accenture.com>
2015-09-02 17:53:34 +0800

    JAM 9/2: Registration unit testing initial

---
+++ Presentation/Jasmine/Spec/RegistrationControllerSpec.js
@@ -0,0 +1,12 @@
+﻿/// <reference path="../../Scripts/angular.min.js" />
+/// <reference path="../../Scripts/angular-mocks.js" />
+/// <reference path="../lib/jasmine.js" />
+/// <reference path="../../Scripts/AngularPrj/LoanApp.js" />
+/// <reference path="../../Scripts/AngularPrj/Service/AspNetUserService.js" />
+/// <reference path="../../Scripts/AngularPrj/Controller/Registration.Controller.js" />
+
+describe('Register Controller', function () {
+    beforeEach(module('LoanApp'));
+
+    
+});
