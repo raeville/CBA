2f6042d9fbf57a98e689f00d3932cee1ca53d539
Medenilla, Jay A. H. <jay.a.h.medenilla@accenture.com>
2015-09-03 14:17:31 +0800

    JAM 9/3: Changes in Login controller

--- Presentation/Scripts/AngularPrj/Controller/LogIn.Controller.js
+++ Presentation/Scripts/AngularPrj/Controller/LogIn.Controller.js
@@ -1,27 +1,23 @@
-﻿LoanApp.controller('LoginController',  ['$scope', 'AspNetUser', function ($scope, AspNetUser) {
+﻿LoanApp.controller('LoginController', ['$scope', 'AspNetUser', '$location', function ($scope, AspNetUser, $location) {
 
     $scope.signIn = function () {
         var user = { };
         var username = $scope.user.username;
         var password = $scope.user.password;
 
-
         var user = {
             "username": username,
             "password": password
         };
 
-        AspNetUser.login(user).
-            then(function (response) {
-                console.log(response);
-                $('#myModal').modal('hide');
-                $('#myModal').on('hidden.bs.modal', function (e) { $(this).find('input').val('').end() })
-                $location.path("/loanCalculator");
+        AspNetUser.login(user).success(function (response) {
+            $('#myModal').modal('hide');
+            $('#myModal').on('hidden.bs.modal', function (e) { $(this).find('input').val('').end() })
+            $location.path("/loanCalculator");
 
-            }, function (response) {
-                console.log(response)
-                $scope.IncorrectPassword = response.error_description;
-            });
+        }).error(function (response) {
+            $scope.IncorrectPassword = response.error_description;
+        });
     };
   $scope.ClearMessage = function ()
     {
