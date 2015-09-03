94d6b75aa567dbd15f56827c399bd1be8ec022c7
Medenilla, Jay A. H. <jay.a.h.medenilla@accenture.com>
2015-09-03 13:56:09 +0800

    JAM 9/3: Unit testing for Register controller

--- Presentation/Jasmine/Spec/RegistrationControllerSpec.js
+++ Presentation/Jasmine/Spec/RegistrationControllerSpec.js
@@ -5,8 +5,40 @@
 /// <reference path="../../Scripts/AngularPrj/Service/AspNetUserService.js" />
 /// <reference path="../../Scripts/AngularPrj/Controller/Registration.Controller.js" />
 
-describe('Register Controller', function () {
+describe("Register Controller", function () {
+
+    //Arrange;
+    var scope;
+    var controller;
+    var service;
+
     beforeEach(module('LoanApp'));
 
-    
+    beforeEach(angular.mock.inject(function ($controller, $rootScope, $http, $provide) {
+        mockScope = $rootScope.$new();
+        service = jasmine.createSpyObj("service", ["registerUser"]);
+        service.registerUser.and.callThrough();
+        $provide.value("service", service);
+
+        $controller('RegistrationController', {
+            $scope: scope,
+            $http: $http
+        });
+
+
+        it("Should retrieve the customer API", function () {
+            var expectedURL = {
+                apiUrl: 'http://localhost:51361/'
+            };
+            $http.expectGET(expectedURL.apiUrl);
+        });
+
+        it("should call the register method on AspNetUser", function () {
+            expect(service.registerUser).toHaveBeenCalled();
+        });
+        
+    }));
+
+
+
 });
