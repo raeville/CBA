0800fddec02c437f4fa520d500f96a9ce80a2ef2
atbelarmino <Gr@phite9441>
2015-09-04 14:29:34 +0800

    Updated Customer table fields.

--- LoanApi/Models/CustomerModel.cs
+++ LoanApi/Models/CustomerModel.cs
@@ -1,11 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
 namespace LoanApi.Models
 {
-    public class CustomerModel
+    public class Customer : BaseModel
     {
+        [EmailAddress]
+        [MaxLength(20)]
+        [Key]
+        public string Email { get; set; }
+        [MaxLength(20)]
+        public string FirstName { get; set; }
+        [MaxLength(20)]
+        public string LastName { get; set; }
+        [MaxLength(1)]
+        public string MiddleName { get; set; }
+        //type "Male/Female"
+        [MaxLength(1)]
+        public string Gender { get; set; }
+        [MaxLength(150)]
+        public string Address { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime BirthDate { get; set; }
+        //type "Single/Married/Legally Separated/Divorced/Widowed"
+        [MaxLength(1)]
+        public string MaritalStatus { get; set; }
+        //type "Business/Employment"
+        [MaxLength(10)]
+        public string SourceOfIncome { get; set; }
+        //true - inactive / false -  active
+        public bool IsDeleted { get; set; }
     }
 }
