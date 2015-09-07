5d051e529850a82bd6aa9ab6573d4a17f748bd65
Medenilla, Jay A. H. <jay.a.h.medenilla@accenture.com>
2015-09-07 11:47:32 +0800

    JAM 9/7: Unit testing for Register controller

--- Presentation/Jasmine/Spec/RegistrationControllerSpec.js
+++ Presentation/Jasmine/Spec/RegistrationControllerSpec.js
@@ -9,10 +9,45 @@ describe('Registration Controller', function () {
 
     var regCtrl;
     var scope;
-    var service;
+    var mockService;
     var q;
     var deferred;
     var user;
     var $rootScope;
+    
+    beforeEach(function () {
+        user = {
+            "Email": "test5@cba.com",
+            "Password": "Password1!",
+            "ConfirmPassword": "Password1!"
+        }
+        mockService = {
+            registerUser: function () {
+                deferred = q.defer();
+                return deferred.promise;
+            }
+        };
+    });
+
+    beforeEach(inject(['$rootScope', '$controller', '$q', function ($rootScope, $controller, $q) {
+        q = $q;
+        scope = $rootScope.$new();
+        scope.user = user;
+
+        loginCtrl = $controller('RegistrationController', {
+            $scope: scope,
+            AspNetUser: mockService
+        });
+    }]));
+
+    it('should post to AspNetUser.register service when registerUser is called', function () {
+        spyOn(mockService, 'registerUser').and.callThrough();        
+        scope.registerUser();
+        deferred.resolve(user);
+        scope.$root.$digest();
+        expect(mockService.registerUser).toHaveBeenCalled();
+    });
+
+
     
 });
