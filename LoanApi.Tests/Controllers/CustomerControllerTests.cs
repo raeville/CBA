6ed75c1be7b7f776fbd1e76da06ba2a21a8b0b75
Jay Albert Medenilla <jay.albert.medenilla@hotmail.com>
2015-09-08 19:43:58 +0800

    JAM 9/8: Initial for Customer api unit test

---
+++ LoanApi.Tests/Controllers/CustomerControllerTests.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Web.Http;
+using System.Threading.Tasks;
+using System.Web.Http.Results;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LoanApi;
+using LoanApi.Models;
+using LoanApi.Controllers;
+
+namespace LoanApi.Tests.Controllers
+{
+    [TestClass]
+    public class CustomerControllerTests
+    {
+
+        [TestMethod]
+        public void CustomerGet()
+        {
+
+            // Arrange
+            ValuesController controller = new ValuesController();
+
+            // Act
+            IEnumerable<string> result = controller.Get();
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(2, result.Count());
+            Assert.AreEqual("value1", result.ElementAt(0));
+            Assert.AreEqual("value2", result.ElementAt(1));
+        }
+
+        [TestMethod]
+        public void CustomerGetById()
+        {
+            // Arrange
+            ValuesController controller = new ValuesController();
+
+            // Act
+            string result = controller.Get(5);
+
+            // Assert
+            Assert.AreEqual("value", result);
+        }
+
+        [TestMethod]
+        public void CustomerPost()
+        {
+            // Arrange
+            ValuesController controller = new ValuesController();
+
+            // Act
+            controller.Post("value");
+
+            // Assert
+        }
+
+        [TestMethod]
+        public void CustomerPut()
+        {
+            // Arrange
+            ValuesController controller = new ValuesController();
+
+            // Act
+            controller.Put(5, "value");
+
+            // Assert
+        }
+
+        [TestMethod]
+        public void CustomerDelete()
+        {
+            // Arrange
+            ValuesController controller = new ValuesController();
+
+            // Act
+            controller.Delete(5);
+
+            // Assert
+        }
+    }
+}
