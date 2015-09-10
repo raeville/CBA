65e48d7918b7321bc23c0cc577e3a9b45de0f2b5
Raymund <atbelarmino/Gr@phite9441>
2015-09-10 10:10:13 +0800

    update loacl storage specs for email and token

--- Presentation/Jasmine/Spec/LocalStorageServiceSpec.js
+++ Presentation/Jasmine/Spec/LocalStorageServiceSpec.js
@@ -13,8 +13,8 @@ describe('Local Storage Service Unit Tests', function () {
     var User;
     beforeEach(function () {
       var  user = {
-            "Email": "test5@cba.com"
-            //"Password": "Password1!"
+            "Email": "test5@cba.com",
+            "access_token": "2974LErhmJIlyeewjp34lmfZaBpl"
       }
       User = user;
     });
@@ -26,23 +26,49 @@ describe('Local Storage Service Unit Tests', function () {
     it('should have a Local Storage service to be defined', function () {
         expect(localstorage).toBeDefined();
     });
+    //Email
     it('should not have a existing Email upon starting up', function () {
         localstorage.remove('Email');
         expect(localstorage.get('Email')).toBeUndefined();
     });
-    it('should Set and Get an Email', function () {
+    //Token
+    it('should not have a existing Token upon starting up', function () {
+        localstorage.remove('Token');
+        expect(localstorage.get('Token')).toBeUndefined();
+    });
+    //Email
+    it('should Set and Get an Email in the local storage', function () {
         localstorage.set('Email', User.Email);
         expect(localstorage.get('Email')).toBe(User.Email);
     });
+    //Token
+    it('should Set and Get a Token in the local storage', function () {
+        localstorage.set('Token', User.access_token);
+        expect(localstorage.get('Token')).toBe(User.access_token);
+    });
+    //Email
     it('should have an Email in local storage after calling Set Local Storage', function () {
         localstorage.set('Email', User.Email);
         expect(localstorage.get('Email')).toBe(User.Email);
     });
-
+    //Token
+    it('should have a Token in local storage after calling Set Local Storage', function () {
+        localstorage.set('Token', User.access_token);
+        expect(localstorage.get('Token')).toBe(User.access_token);
+    });
+    //Email
     it('should remove the Email from local storage after logging out', function () {
         localstorage.set('Email', User.Email);
         expect(localstorage.get('Email')).toBe(User.Email);
         localstorage.remove('Email');
         expect(localstorage.get('Email')).toBeUndefined();
     });
+    //Token
+    it('should remove the Token from local storage after logging out', function () {
+        localstorage.set('Token', User.access_token);
+        expect(localstorage.get('Token')).toBe(User.access_token);
+        localstorage.remove('Token');
+        expect(localstorage.get('Token')).toBeUndefined();
+    });
+
 });
--- Presentation/Scripts/AngularPrj/Controller/LogIn.Controller.js
+++ Presentation/Scripts/AngularPrj/Controller/LogIn.Controller.js
@@ -1,23 +1,28 @@
 ﻿LoanApp.controller('LoginController', ['$scope', 'AspNetUser', '$location', '$localstorage', '$window', function ($scope, AspNetUser, $location, $localstorage, $window) {
-
+   
     $scope.signIn = function () {
         var user = {};
      
         var username = $scope.user.username;
         var password = $scope.user.password;
-
+        var access_token = $scope.user.access_token;
         var user = {
             "username": username,
-            "password": password
+            "password": password,
+            "access_token": access_token
         };
 
         AspNetUser.login(user).then(function (response) { 
             if ($scope.user.username != "") {
                 //// Set Email and Token in $localstorage
                 $localstorage.set('Email', $scope.user.username);
-                $localstorage.set('Token', response.data.access_token);
-
-                angular.element('#myModal').modal('hide');
+                if ($scope.user.access_token != "") {
+                    $localstorage.set('Token', $scope.user.access_token);
+                }
+                else {
+                    $localstorage.set('Token', response.data.access_token);
+                }
+             angular.element('#myModal').modal('hide');
                 $window.location.reload();
                 $location.path("/loanCalculator");
             }
