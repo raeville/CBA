534c64425495f79f425120543b8e5433cb3446d5
Raymund <atbelarmino/Gr@phite9441>
2015-09-11 12:28:53 +0800

    fix log-in specs

--- Presentation/Scripts/AngularPrj/Controller/LogIn.Controller.js
+++ Presentation/Scripts/AngularPrj/Controller/LogIn.Controller.js
@@ -1,33 +1,28 @@
 ﻿LoanApp.controller('LoginController', ['$scope', 'AspNetUser', '$location', '$localstorage', '$window', function ($scope, AspNetUser, $location, $localstorage, $window) {
-   
+
     $scope.signIn = function () {
         var user = {};
-     
+
         var username = $scope.user.username;
         var password = $scope.user.password;
-        var access_token = $scope.user.access_token;
         var user = {
             "username": username,
             "password": password,
-            "access_token": access_token
+            "access_token": null
         };
-
-        
         AspNetUser.login(user).then(function (response) {
-            console.log(user);
-            console.log(response);
-            console.log(response.data);
+
             if ($scope.user.username != "") {
                 //// Set Email and Token in $localstorage
                 $localstorage.set('username', $scope.user.username);
-                if ($scope.user.access_token != "") {
+                if (angular.isObject($scope.user.access_token) == true || $scope.user.access_token != null) {
                     $localstorage.set('access_token', $scope.user.access_token);
                 }
                 else {
                     $localstorage.set('access_token', response.data.access_token);
                 }
-             angular.element('#myModal').hide();
-               $window.location.reload();
+                angular.element('#myModal').hide();
+                $window.location.reload();
                 $location.path("/loanCalculator");
             }
             $scope.user.username = "";
@@ -36,11 +31,9 @@
         }).catch(function (response) {
             if ($scope.user.username != "" && $scope.user.password != "")
             { $scope.IncorrectPassword = response.data.error_description; }
-        
         });
     };
     $scope.ClearMessage = function () {
         $scope.IncorrectPassword = "";
     };
-
 }]);
--- Presentation/Scripts/AngularPrj/Controller/Registration.Controller.js
+++ Presentation/Scripts/AngularPrj/Controller/Registration.Controller.js
@@ -13,22 +13,18 @@ LoanApp.controller('RegistrationController', ['$scope', 'AspNetUser', '$location
             "Password": password,
             "ConfirmPassword": confirmpassword
         }
-
         AspNetUser.registerUser(userData).then(function (response) {
             $scope.userData.inputEmail = "";
             $scope.userData.inputPassword = "";
             $scope.userData.inputConfirmPassword = "";
             angular.element('#myModalReg').hide();
             $location.path("/RegisterSuccessful");
-            //$window.location.reload();
-           
+           $window.location.reload();           
         }).catch(function (response) {
             if ($scope.userData.inputEmail.length != 0 && $scope.userData.inputPassword.length != 0) { $scope.Message = response.modelState[""][1]; }
-
         });
     };
     $scope.ClearMessage = function () {
         $scope.Message = "";
     };
-
 }]);
