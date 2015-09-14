30916fabcd20f40982857c89547537389b6b555c
Clyde <clyde.uyenghua@live.com>
2015-09-14 13:45:37 +0800

    Fixed angular js error on directives and footer error.

--- Presentation/Scripts/AngularPrj/Directives/GetMyEmail.Directives.js
+++ Presentation/Scripts/AngularPrj/Directives/GetMyEmail.Directives.js
@@ -2,13 +2,15 @@
 // Retrieve email address of logged user
 LoanApp.directive('myEmail', function ($localstorage) {
 
+    var message = "";
     if ($localstorage.get('username') != null) {
         angular.element('#signin').html('');
         angular.element('#logout').html('<a href="#" ng-click="LogOut()">Log out</a>');
-        return {
-            template: 'Welcome, ' + $localstorage.get('username')
-        };
+        message = 'Welcome, ' + $localstorage.get('username');
     }
+    return {
+        template: message
+    };
 });
 
 
