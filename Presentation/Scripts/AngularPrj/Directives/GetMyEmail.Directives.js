a6ea6f85d2b4734c02982ca166ea229fe0ebdb28
Rue Rae <raeville7@gmail.com>
2015-09-17 19:01:52 +0800

    showhide myprofile tab

--- Presentation/Scripts/AngularPrj/Directives/GetMyEmail.Directives.js
+++ Presentation/Scripts/AngularPrj/Directives/GetMyEmail.Directives.js
@@ -1,12 +1,14 @@
-﻿/// <reference path="../Service/LocalStorage.Service.js" />
+﻿/// <reference path="../Service/$localstorage.Service.js" />
 // Retrieve email address of logged user
 LoanApp.directive('myEmail', function ($localstorage) {
 
     var message = "";
     if ($localstorage.get('username') != null) {
+
         angular.element('#signin').html('');
         angular.element('#logout').html('<a href="#" ng-click="LogOut()">Log out</a>');
         message = 'Welcome, ' + $localstorage.get('username');
+        angular.element('#MyProfile').html('<a href="#/MyProfile" ng-show="true" class="list-group-item">My Profile</a>');
     }
     return {
         template: message
