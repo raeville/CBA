2666dcfdcf64d1a6f7390f0e2603b1a3837d2b95
Raymund <atbelarmino/Gr@phite9441>
2015-09-23 10:51:09 +0800

    Added loan type table class

---
+++ LoanApi/Models/LoanTypeModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace LoanApi.Models
+{
+     [Table("LoanTyp")]
+    public class LoanTypeModel : BaseModel
+    {
+         [MaxLength(20)]
+         public string TypeDesc { get; set; }
+    }
+}
