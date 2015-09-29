b7b6219c9325a241ad8d7f5e7b5fc9c328ac9577
Jay Albert Medenilla <jay.albert.medenilla@hotmail.com>
2015-09-29 18:29:00 +0800

    JAM 9/28: save for maritatus value was revised to get the value of chosen marital status

--- Presentation/Scripts/AngularPrj/Controller/Customer.Controller.js
+++ Presentation/Scripts/AngularPrj/Controller/Customer.Controller.js
@@ -75,7 +75,7 @@ LoanApp.controller('CustomerController', ['$scope', '$location', 'CustomerServic
             Gender:         $scope.gender,
             Address:        $scope.homeAddress,
             BirthDate:      new Date($scope.birthDate),
-            MaritalStatus:  $scope.maritalstatus[0].value,
+            MaritalStatus: $scope.maritalStatus,
             SourceOfIncome: $scope.sourceOfIncome
         };
 
